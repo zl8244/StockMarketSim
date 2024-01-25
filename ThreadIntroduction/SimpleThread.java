89185f4238e635954b795a6e5bbd0ad654cb7e6c
zl8244 <zl8244@g.rit.edu>
2024-01-25 11:12:04 -0500

    Program appears to be working

--- ThreadIntroduction/SimpleThread.java
+++ ThreadIntroduction/SimpleThread.java
@@ -1,5 +1,23 @@
 package ThreadIntroduction;
 
-public class SimpleThread {
+public class SimpleThread extends Thread{
+
+    private int count;
+    private String name;
+
+    public SimpleThread(String name) {
+        this.name = name;
+        count = 0;
+    }
     
+    public void run() {
+        while(count < 9) {
+            count++;
+            if(count % 2 == 0) {
+                Thread.yield();
+            }
+            System.out.println("" + count + " " + name);
+        }
+        System.out.println("Done! " + name);
+    }
 }
--- ThreadIntroduction/TwoThreadTest.java
+++ ThreadIntroduction/TwoThreadTest.java
@@ -1,5 +1,11 @@
 package ThreadIntroduction;
 
 public class TwoThreadTest {
-    
+
+    public static void main(String[] args) {
+        SimpleThread Hi = new SimpleThread("Hi");
+        SimpleThread Ho = new SimpleThread("Ho");
+        Hi.start();
+        Ho.start();
+    }
 }
