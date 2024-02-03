7ace4f88a5410d648e1a2d497bb5cdd2d3dd30d9
zl8244 <145813072+ZhiweiLiu03@users.noreply.github.com>
2024-02-03 12:26:50 -0500

    Bridge queue seems to be working

--- activity_4/Bridge.java
+++ activity_4/Bridge.java
@@ -1,18 +1,22 @@
 package activity_4;
 
+import java.util.LinkedList;
+
 public class Bridge {
 
     private volatile int num;
+    private volatile LinkedList<Thread> queue = new LinkedList<>();
     
     public synchronized void enterBridge() {
-        while(num == 3) {
+        queue.add(Thread.currentThread());
+        while(num == 3 || !Thread.currentThread().equals(queue.getFirst())) {
             try {
                 wait();
             } catch (InterruptedException e) {
             }
         }
         num++;
-        notifyAll();
+        queue.remove();
     }
 
     public synchronized void leaveBridge() {
