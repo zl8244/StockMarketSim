9f8f9ee2d236b3497e42d944bd52a263d252571d
Zhiwei Liu <zl8244@rit.edu>
2024-03-01 09:09:39 -0500

    Initial commit

---
+++ timers_demo/signal_interrupt_timer.c
@@ -0,0 +1,99 @@
+// Source: https://opensource.com/article/21/10/linux-timers
+//
+// Description from above source
+// Another possibility to be notified by an expired timer is based
+// on a kernel signal. Instead of creating a new thread each time
+// the timer expires, the kernel sends a signal to the process,
+// the process is interrupted, and the corresponding signal handler is called.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define UNUSED(x) (void)(x)
+
+static void handler(int sig, siginfo_t *si, void *uc);
+pid_t gettid(void);
+
+struct t_eventData{
+    int myData;
+};
+
+int main()
+{
+    int res = 0;
+    timer_t timerId = 0;
+
+
+    struct sigevent sev = { 0 };
+    struct t_eventData eventData = { .myData = 0 };
+
+    /* specifies the action when receiving a signal */
+    struct sigaction sa = { 0 };
+
+    /* specify start delay and interval */
+    struct itimerspec its = {   .it_value.tv_sec  = 1,
+                                .it_value.tv_nsec = 0,
+                                .it_interval.tv_sec  = 1,
+                                .it_interval.tv_nsec = 0
+                            };
+
+    printf("Signal Interrupt Timer - thread-id: %d\n", gettid());
+
+    sev.sigev_notify = SIGEV_SIGNAL; // Linux-specific
+    sev.sigev_signo = SIGRTMIN;
+    sev.sigev_value.sival_ptr = &eventData;
+
+    /* create timer */
+    res = timer_create(CLOCK_REALTIME, &sev, &timerId);
+
+    if ( res != 0){
+        fprintf(stderr, "Error timer_create: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    /* specifz signal and handler */
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handler;
+
+    /* Initialize signal */
+    sigemptyset(&sa.sa_mask);
+
+    printf("Establishing handler for signal %d\n", SIGRTMIN);
+
+    /* Register signal handler */
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1){
+        fprintf(stderr, "Error sigaction: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    /* start timer */
+    res = timer_settime(timerId, 0, &its, NULL);
+
+    if ( res != 0){
+        fprintf(stderr, "Error timer_settime: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    printf("Press ENTER to Exit\n");
+    while(getchar()!='\n'){}
+    return 0;
+}
+
+
+
+static void
+handler(int sig, siginfo_t *si, void *uc)
+{
+    UNUSED(sig);
+    UNUSED(uc);
+    struct t_eventData *data = (struct t_eventData *) si->_sifields._rt.si_sigval.sival_ptr;
+    printf("Timer fired %d - thread-id: %d\n", ++data->myData, gettid());
+}
