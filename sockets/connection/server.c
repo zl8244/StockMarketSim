6e9a307157486a854101f0b0948017230f1b454c
zl8244 <zl8244@g.rit.edu>
2024-04-22 09:41:55 -0400

    Undo

--- sockets/connection/server.c
+++ sockets/connection/server.c
@@ -36,22 +36,20 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     for(int i = 0; i < 1000; i++) {
-        listen(sockfd,5);
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd, 
-                    (struct sockaddr *) &cli_addr, 
-                    &clilen);
-        if (newsockfd < 0) 
-            error("ERROR on accept");
-        bzero(buffer,256);
-        n = read(newsockfd,buffer,255);
-        if (n < 0) error("ERROR reading from socket");
-        printf("Here is the message: %s\n",buffer);
-        n = write(newsockfd,"I got your message",18);
-        if (n < 0) error("ERROR writing to socket");
-        close(newsockfd);
-     }
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     close(newsockfd);
      close(sockfd);
      return 0; 
 }
