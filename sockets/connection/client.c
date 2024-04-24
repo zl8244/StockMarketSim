f3d8e25bbdfb81dfdd241985c0376eb0bdef9e17
Zhiwei Liu <zl8244@rit.edu>
2024-04-24 12:40:19 -0400

    message now auto set

--- sockets/connection/client.c
+++ sockets/connection/client.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
+    char buffer[256] = "qwertyuiop\0";
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -42,9 +42,9 @@ int main(int argc, char *argv[])
         serv_addr.sin_port = htons(portno);
         if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
             error("ERROR connecting");
-        printf("Please enter the message: ");
-        bzero(buffer,256);
-        fgets(buffer,255,stdin);
+        //printf("Please enter the message: ");
+        //bzero(buffer,256);
+        //fgets(buffer,255,stdin);
         n = write(sockfd,buffer,strlen(buffer));
         if (n < 0) 
             error("ERROR writing to socket");
